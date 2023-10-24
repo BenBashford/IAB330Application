03849f247ac5044c3fffcd4deb1513f18876ccae
BenBashford <75756448+BenBashford@users.noreply.github.com>
2023-10-24 13:31:02 +1000

    Fixed Version

--- app/src/main/java/com/example/navigationprototype/ServicesAdapter.java
+++ app/src/main/java/com/example/navigationprototype/ServicesAdapter.java
@@ -1,8 +1,8 @@
 package com.example.navigationprototype;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
